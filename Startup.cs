774d3f53572b47645b2c9fcf33039af532aa8b04
frank <frank.haugen@gmail.com>
2019-03-11 18:42:20 +0100

    Cleanup

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-//using Microsoft.Extensions.Hosting;
 
 namespace scheduled_job
 {
@@ -17,7 +16,6 @@ namespace scheduled_job
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddNewtonsoftJson();
@@ -27,7 +25,6 @@ namespace scheduled_job
 			services.AddTransient<ScheduledJob>();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
